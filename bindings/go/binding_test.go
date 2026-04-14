a9b78d9370bfc60a58de08417c09622051cac049
vlicecream <vlicecream@163.com>
2026-04-14 11:35:10 +0800

    Complete Grammar.js And generate

---
+++ bindings/go/binding_test.go
@@ -0,0 +1,15 @@
+package tree_sitter_unreal_cpp_test
+
+import (
+	"testing"
+
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_unreal_cpp "github.com/vlicecream/utreesitter/bindings/go"
+)
+
+func TestCanLoadGrammar(t *testing.T) {
+	language := tree_sitter.NewLanguage(tree_sitter_unreal_cpp.Language())
+	if language == nil {
+		t.Errorf("Error loading Unreal c++ grammar")
+	}
+}
